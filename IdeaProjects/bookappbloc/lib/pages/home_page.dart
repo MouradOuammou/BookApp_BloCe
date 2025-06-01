566ef5cfcac6421e4b3ef8fdac7516ed44852a9d
Mourad Ouammou <mouradouammou8@gmail.com>
2025-06-01 11:05:13 +0100

    Home Page

---
+++ IdeaProjects/bookappbloc/lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Book Search')),
+      body: Column(
+        children: [
+          SearchBar(),
+          Expanded(
+            child: BlocBuilder<BookBloc, BookState>(
+              builder: (context, state) {
+                if (state is BookInitial) {
+                  return Center(child: Text('Recherchez un livre'));
+                } else if (state is BookLoading) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (state is BookError) {
+                  return Center(child: Text(state.message));
+                } else if (state is BookLoaded) {
+                  return BookList(books: state.books);
+                }
+                return Container();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

93f6dbd715af46d9ad707883c81ee4842c768823
Mourad Ouammou <mouradouammou8@gmail.com>
2025-06-01 11:29:13 +0100

    fixer 7

--- IdeaProjects/bookappbloc/lib/pages/favorites_page.dart
+++ IdeaProjects/bookappbloc/lib/pages/favorites_page.dart
@@ -1,30 +1,48 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../blocs/favorite_bloc.dart';
+import '../blocs/favorite_event.dart';
 import '../blocs/favorite_state.dart';
-import '../widgets/book_card.dart';
+import '../widgets/book_list.dart';
+
 class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Favoris')),
-      body: BlocBuilder<FavoriteBloc, FavoriteState>(
+      appBar: AppBar(title: const Text('Favoris')),
+      body: BlocConsumer<FavoriteBloc, FavoriteState>(
+        listener: (context, state) {
+          if (state is FavoriteError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.message)),
+            );
+          }
+        },
         builder: (context, state) {
           if (state is FavoriteLoading) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (state is FavoriteLoaded) {
             return state.books.isEmpty
-                ? Center(child: Text('Aucun favori'))
-                : GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              itemCount: state.books.length,
-              itemBuilder: (context, index) {
-                return BookCard(
-                  book: state.books[index],
-                  isFavorite: true,
+                ? const Center(child: Text('Aucun favori'))
+                : BookList(
+              books: state.books,
+              favoriteStatuses: {for (var book in state.books) book.id: true},
+              onToggleFavorite: (book) {
+                context.read<FavoriteBloc>().add(
+                  ToggleFavorite(book, true),
+                );
+              },
+            );
+          } else if (state is FavoriteUpdated) {
+            return BookList(
+              books: state.books,
+              favoriteStatuses: {for (var book in state.books) book.id: true},
+              onToggleFavorite: (book) {
+                context.read<FavoriteBloc>().add(
+                  ToggleFavorite(book, true),
                 );
               },
             );

97a4e618013a37e303c187d1aa0043b3eb2ec0ee
Mourad Ouammou <mouradouammou8@gmail.com>
2025-06-01 11:04:32 +0100

    model

---
+++ IdeaProjects/bookappbloc/lib/data/models/book.dart
@@ -0,0 +1,40 @@
+class Book {
+  final String id;
+  final String title;
+  final String author;
+  final String imageUrl;
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.imageUrl,
+  });
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      id: json['id'],
+      title: json['volumeInfo']['title'] ?? 'Titre inconnu',
+      author: json['volumeInfo']['authors']?.join(', ') ?? 'Auteur inconnu',
+      imageUrl: json['volumeInfo']['imageLinks']?['thumbnail'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'author': author,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  factory Book.fromMap(Map<String, dynamic> map) {
+    return Book(
+      id: map['id'],
+      title: map['title'],
+      author: map['author'],
+      imageUrl: map['imageUrl'],
+    );
+  }
+}

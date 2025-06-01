43dad5fac578126cfeda570340bb65de7211c9be
Mourad Ouammou <mouradouammou8@gmail.com>
2025-06-01 11:32:47 +0100

    all fixer

--- IdeaProjects/bookappbloc/lib/main.dart
+++ IdeaProjects/bookappbloc/lib/main.dart
@@ -1,25 +1,29 @@
-import 'package:bookappbloc/pages/favorites_page.dart';
-import 'package:bookappbloc/pages/home_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'blocs/bloc_providers.dart';
+import 'pages/favorites_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Book App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: BlocProviders(
-        child: HomePage(),
+        child: const HomePage(),
       ),
       routes: {
-        '/favorites': (context) => FavoritesPage(),
+        '/favorites': (context) => const FavoritesPage(),
       },
     );
   }

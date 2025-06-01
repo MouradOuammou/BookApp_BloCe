2cee25481b3ceb45fa1dac7087d581c8151980df
Mourad Ouammou <mouradouammou8@gmail.com>
2025-06-01 11:08:31 +0100

    Fichiers BLoC pour les favoris

---
+++ IdeaProjects/bookappbloc/lib/blocs/favorite_event.dart
@@ -0,0 +1,22 @@
+abstract class FavoriteEvent {}
+
+class LoadFavorites extends FavoriteEvent {}
+
+class AddFavorite extends FavoriteEvent {
+  final Book book;
+
+  AddFavorite(this.book);
+}
+
+class RemoveFavorite extends FavoriteEvent {
+  final String bookId;
+
+  RemoveFavorite(this.bookId);
+}
+
+class ToggleFavorite extends FavoriteEvent {
+  final Book book;
+  final bool isFavorite;
+
+  ToggleFavorite(this.book, this.isFavorite);
+}
